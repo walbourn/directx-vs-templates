2f536c2a63dff7190181b0dd7b8e7fb9338236d2
Chuck Walbourn <chuckw@microsoft.com>
2015-06-24 15:06:28 -0700

    Direct3D Game template for VS 2015

---
+++ Win32Game_2015/Game.h
@@ -0,0 +1,69 @@
+//
+// Game.h
+//
+
+#pragma once
+
+#include "pch.h"
+#include "StepTimer.h"
+
+// A basic game implementation that creates a D3D11 device and
+// provides a game loop
+class Game
+{
+public:
+
+    Game();
+
+    // Initialization and management
+    void Initialize(HWND window, int width, int height);
+
+    // Basic game loop
+    void Tick();
+    void Render();
+
+    // Rendering helpers
+    void Clear();
+    void Present();
+
+    // Messages
+    void OnActivated();
+    void OnDeactivated();
+    void OnSuspending();
+    void OnResuming();
+    void OnWindowSizeChanged(int width, int height);
+
+    // Properites
+    void GetDefaultSize( int& width, int& height ) const;
+
+private:
+
+    void Update(DX::StepTimer const& timer);
+
+    void CreateDevice();
+    void CreateResources();
+    
+    void OnDeviceLost();
+
+    // Application state
+    HWND                                            m_window;
+    int                                             m_outputWidth;
+    int                                             m_outputHeight;
+
+    // Direct3D Objects
+    D3D_FEATURE_LEVEL                               m_featureLevel;
+    Microsoft::WRL::ComPtr<ID3D11Device>            m_d3dDevice;
+    Microsoft::WRL::ComPtr<ID3D11Device1>           m_d3dDevice1;
+    Microsoft::WRL::ComPtr<ID3D11DeviceContext>     m_d3dContext;
+    Microsoft::WRL::ComPtr<ID3D11DeviceContext1>    m_d3dContext1;
+
+    // Rendering resources
+    Microsoft::WRL::ComPtr<IDXGISwapChain>          m_swapChain;
+    Microsoft::WRL::ComPtr<IDXGISwapChain1>         m_swapChain1;
+    Microsoft::WRL::ComPtr<ID3D11RenderTargetView>  m_renderTargetView;
+    Microsoft::WRL::ComPtr<ID3D11DepthStencilView>  m_depthStencilView;
+    Microsoft::WRL::ComPtr<ID3D11Texture2D>         m_depthStencil;
+
+    // Game state
+    DX::StepTimer                                   m_timer;
+};
