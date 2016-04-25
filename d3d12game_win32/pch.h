42c0ff0960b8d28365a28f7f4ff957540f16f01d
Chuck Walbourn <chuckw@windows.microsoft.com>
2016-04-24 20:45:13 -0700

    Minimized windows.h content

--- d3d12game_win32/pch.h
+++ d3d12game_win32/pch.h
@@ -12,6 +12,20 @@
 // Use the C++ standard templated min/max
 #define NOMINMAX
 
+// DirectX apps don't need GDI
+#define NODRAWTEXT
+#define NOGDI
+#define NOBITMAP
+
+// Include <mcx.h> if you need this
+#define NOMCX
+
+// Include <winsvc.h> if you need this
+#define NOSERVICE
+
+// WinHelp is deprecated
+#define NOHELP
+
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
