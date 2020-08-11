ce92141b0c9109f9a03ec7a2b9d5f7a8084023ec
Chuck Walbourn <chuckw@windows.microsoft.com>
2020-08-10 23:42:18 -0700

    Added cmake helper powershell script

---
+++ VSIX/createcmake.ps1
@@ -0,0 +1,70 @@
+<#
+
+.SYNOPSIS
+This script creates a instance of one of the Win32 templates using CMake
+
+.DESCRIPTION
+This script is intended for creating a template instance outside of Visual Studio using CMake for building instead.
+
+.PARAMETER templatedir
+Provides the directory to read for the source template (d3d11game_win32, etc.)
+
+.PARAMETER projectname
+This is name of the project (such as "Direct3DGame")
+
+.PARAMETER targetdir
+This is the name of the target directory for the project (must not already exist)
+
+.EXAMPLE
+createcmake ../d3d11game_win32 Direct3DGame $Env:USERPROFILE\source\Direct3DGame
+
+.LINK
+https://github.com/walbourn/directx-vs-templates/wiki
+
+#>
+
+param (
+    [string]$templatedir,
+    [string]$projectname,
+    [string]$targetdir
+)
+
+$cmake = $templatedir + "\CMakeLists.txt"
+
+
+if (-not (Test-Path -Path $cmake)) {
+    Write-Error -Message "$templatedir does not contain a CMake" -ErrorAction Stop
+}
+
+if (-not (Test-Path $targetdir)) {
+    try {
+        New-Item -Path $targetdir -ItemType Directory -ErrorAction Stop | Out-Null
+    }
+    catch {
+        Write-Error -Message "Unable to create project directory '$targetdir'`n $_" -ErrorAction Stop
+    }
+
+    $files = Get-ChildItem $templatedir | Where {$_.extension -eq ".cpp"} | % { $_.FullName }
+    $files += Get-ChildItem $templatedir | Where {$_.extension -eq ".h"} | % { $_.FullName }
+
+Write-Host $files
+
+    foreach ($file in $files) {
+        $target = $targetdir + "\" + [System.IO.Path]::GetFileName($file)
+Write-Host $target
+        $i = Get-Content $file -Raw
+        $o = $i.Replace("`$projectname$", $projectname)
+        $o = $o.Replace("`$safeprojectname$", $projectname)
+
+        $o | Set-Content -Path $target -NoNewline
+    }
+    
+    Copy-Item ($templatedir + "\CMakeLists.txt") -Destination $targetdir
+    Copy-Item ($templatedir + "\*.json") -Destination $targetdir
+    Copy-Item ($templatedir + "\*.rc") -Destination $targetdir
+    Copy-Item ($templatedir + "\directx.ico") -Destination $targetdir
+    Copy-Item ($templatedir + "\settings.manifest") -Destination $targetdir
+}
+else {
+    "ERROR: Project directory already exists"
+}
