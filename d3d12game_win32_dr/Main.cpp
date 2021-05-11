f837e2cd8d93fddf2637449f032381f3d12511be
Chuck Walbourn <chuckw@microsoft.com>
2021-05-11 13:10:43 -0700

    Added change to support Agility SDK

--- d3d12game_win32_dr/Main.cpp
+++ d3d12game_win32_dr/Main.cpp
@@ -14,6 +14,15 @@ using namespace DirectX;
 
 #pragma warning(disable : 4061)
 
+#ifdef USING_D3D12_AGILITY_SDK
+extern "C"
+{
+    // Used to enable the "Agility SDK" components
+    __declspec(dllexport) extern const UINT D3D12SDKVersion = D3D12_SDK_VERSION;
+    __declspec(dllexport) extern const char* D3D12SDKPath = u8".\\D3D12\\";
+}
+#endif
+
 namespace
 {
     std::unique_ptr<Game> g_game;
