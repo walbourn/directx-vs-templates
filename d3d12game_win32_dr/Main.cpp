9672ffa8a3f7dd876af5875c96e45725b4787ab6
Chuck Walbourn <chuckw@microsoft.com>
2021-06-02 14:33:29 -0700

    Minor code feedback for fullscreen handling

--- d3d12game_win32_dr/Main.cpp
+++ d3d12game_win32_dr/Main.cpp
@@ -249,7 +249,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             }
             else
             {
-                SetWindowLongPtr(hWnd, GWL_STYLE, 0);
+                SetWindowLongPtr(hWnd, GWL_STYLE, WS_POPUP);
                 SetWindowLongPtr(hWnd, GWL_EXSTYLE, WS_EX_TOPMOST);
 
                 SetWindowPos(hWnd, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
