5797b82a108b5568b8609e19a0dd0533d0064ca6
Chuck Walbourn <chuckw@microsoft.com>
2022-03-03 00:42:31 -0800

    Added CMake support for UWP C++/WinRT projects

--- VSIX/createcmake.ps1
+++ VSIX/createcmake.ps1
@@ -35,6 +35,26 @@ This creates a new instance of the Direct3D 12 Game with Device Resources templa
 
 .\createcmake.ps1 d3d12game_win32_dr Direct3D12GameDR $Env:USERPROFILE\source
 
+.EXAMPLE
+This creates a new instance of the Direct3D UWP Game template:
+
+.\createcmake.ps1 d3d11game_uwp_cppwinrt Direct3DUWPGame $Env:USERPROFILE\source
+
+.EXAMPLE
+This creates a new instance of the Direct3D UWP Game with Device Resources template:
+
+.\createcmake.ps1 d3d11game_uwp_cppwinrt_dr Direct3DUWPGameDR $Env:USERPROFILE\source
+
+.EXAMPLE
+This creates a new instance of the Direct3D 12 UWP Game template:
+
+.\createcmake.ps1 d3d12game_uwp_cppwinrt Direct3DUWP12Game $Env:USERPROFILE\source
+
+.EXAMPLE
+This creates a new instance of the Direct3D 12 UWP Game with Device Resources template:
+
+.\createcmake.ps1 d3d12game_uwp_cppwinrt_dr Direct3DUWP12GameDR $Env:USERPROFILE\source
+
 .LINK
 https://github.com/walbourn/directx-vs-templates/wiki
 
@@ -51,40 +71,60 @@ $templatedir = "..\" + $templatedir
 $cmake = $templatedir + "\CMakeLists.txt"
 
 if (-not (Test-Path -Path $cmake)) {
-    Write-Error -Message "$templatedir does not contain a CMake" -ErrorAction Stop
+    Write-Error -Message "ERROR: $templatedir does not contain a CMake" -ErrorAction Stop
 }
 
 $targetdir = $targetdir + "\" + $projectname
 
-if (-not (Test-Path $targetdir)) {
-    try {
-        New-Item -Path $targetdir -ItemType Directory -ErrorAction Stop | Out-Null
-    }
-    catch {
-        Write-Error -Message "Unable to create project directory '$targetdir'`n $_" -ErrorAction Stop
-    }
+if (Test-Path $targetdir) {
+    Write-Error -Message "ERROR: Project directory already exists" -ErrorAction Stop
+}
 
-    $files = Get-ChildItem $templatedir | Where {$_.extension -eq ".cpp"} | % { $_.FullName }
-    $files += Get-ChildItem $templatedir | Where {$_.extension -eq ".h"} | % { $_.FullName }
+try {
+    New-Item -Path $targetdir -ItemType Directory -ErrorAction Stop | Out-Null
+}
+catch {
+    Write-Error -Message "Unable to create project directory '$targetdir'`n $_" -ErrorAction Stop
+}
 
-Write-Host $files
+$files = Get-ChildItem $templatedir | Where {$_.extension -eq ".cpp"} | % { $_.FullName }
+$files += Get-ChildItem $templatedir | Where {$_.extension -eq ".h"} | % { $_.FullName }
 
-    foreach ($file in $files) {
-        $target = $targetdir + "\" + [System.IO.Path]::GetFileName($file)
-Write-Host $target
-        $i = Get-Content $file -Raw
-        $o = $i.Replace("`$projectname$", $projectname)
-        $o = $o.Replace("`$safeprojectname$", $projectname)
+if (Test-Path -Path ($templatedir + "\Package.appxmanifest")) {
+    $files += Get-ChildItem $templatedir | Where {$_.extension -eq ".appxmanifest"} | % { $_.FullName }
+}
 
-        $o | Set-Content -Path $target -NoNewline
-    }
+$guid = New-Guid
+$locale = Get-WinSystemLocale
+
+foreach ($file in $files) {
+    $target = $targetdir + "\" + [System.IO.Path]::GetFileName($file)
+    $i = Get-Content $file -Raw
+    $o = $i.Replace("`$projectname$", $projectname)
+    $o = $o.Replace("`$safeprojectname$", $projectname)
+    $o = $o.Replace("`$guid9$", $guid)
+    $o = $o.Replace("`$XmlEscapedPublisherDistinguishedName$", "CN=$Env:USERNAME")
+    $o = $o.Replace("`$XmlEscapedPublisher$", "$Env:USERNAME")
+    $o = $o.Replace("x-generate", $locale)
+    $o = $o.Replace("`$targetnametoken$", "@PROJECT_NAME@")
+
+    $o | Set-Content -Path $target -NoNewline
+}
     
-    Copy-Item ($templatedir + "\CMakeLists.txt") -Destination $targetdir
-    Copy-Item ($templatedir + "\*.json") -Destination $targetdir
+Copy-Item ($templatedir + "\CMakeLists.txt") -Destination $targetdir
+Copy-Item ($templatedir + "\*.json") -Destination $targetdir
+if (Test-Path -Path ($templatedir + "\settings.manifest")) {
     Copy-Item ($templatedir + "\*.rc") -Destination $targetdir
     Copy-Item ($templatedir + "\directx.ico") -Destination $targetdir
     Copy-Item ($templatedir + "\settings.manifest") -Destination $targetdir
 }
 else {
-    "ERROR: Project directory already exists"
+    New-Item -Path ($targetdir + "\Assets") -ItemType Directory | Out-Null
+    Copy-Item ($templatedir + "\StoreLogo.png") -Destination ($targetdir + "\Assets\StoreLogo.png")
+    Copy-Item ($templatedir + "\Logo.scale-200.png") -Destination ($targetdir + "\Assets\Logo.png")
+    Copy-Item ($templatedir + "\SmallLogo.scale-200.png") -Destination ($targetdir + "\Assets\SmallLogo.png")
+    Copy-Item ($templatedir + "\SplashScreen.scale-200.png") -Destination ($targetdir + "\Assets\SplashScreen.png")
+    Copy-Item ($templatedir + "\WideLogo.scale-200.png") -Destination ($targetdir + "\Assets\WideLogo.png")
 }
+
+Write-Host "New project in $targetdir"
