de8ac8cce16bc75107530f9bd56f9c1c70176993
Chuck Walbourn <chuckw@microsoft.com>
2025-11-14 17:49:59 -0800

    Drop VS 2019 support

--- VSIX/src/Wizards/MyPlatformToolset.cs
+++ VSIX/src/Wizards/MyPlatformToolset.cs
@@ -23,11 +23,6 @@ namespace Wizards
                 replacementsDictionary["$platformtoolset$"] = "v143";
                 replacementsDictionary["$targetplatformversion$"] = "10.0";
             }
-            else if (regRoot.StartsWith(@"SOFTWARE\MICROSOFT\VISUALSTUDIO\16.0"))
-            {
-                replacementsDictionary["$platformtoolset$"] = "v142";
-                replacementsDictionary["$targetplatformversion$"] = "10.0";
-            }
             else
             {
                 MessageBox.Show(string.Format("ERROR: Unrecognized Visual Studio version\n{0}\n{1}", dte2.Version, regRoot),
